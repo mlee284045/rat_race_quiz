60db7af9735717eb97e5fd800eff5a4efee24907
Mike Lee <mike.seung.lee@gmail.com>
2014-09-19 16:22:24 -0700

    Started to work on basic Users functionality

--- first_rocket/urls.py
+++ first_rocket/urls.py
@@ -4,8 +4,21 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'rat_race.views.home', name='home'),
+    url(r'^register/$', 'rat_race.views.register', name='register'),
 
-    # url(r'^blog/', include('blog.urls')),
+
+
+
+
+
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^password_reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
+    url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    'django.contrib.auth.views.password_reset_confirm',
+    name='password_reset_confirm'),
+    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- rat_race/admin.py
+++ rat_race/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from rat_race.models import Runner, Topic, Quiz
 # Register your models here.
+
+
+@admin.register(Runner)
+class RunnerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    pass
